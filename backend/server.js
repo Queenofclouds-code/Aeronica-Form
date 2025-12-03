992962d2e1fb39074e293c088347cf2fdbf5cb75
Queenofclouds-code <meghaj9096@gmail.com>
2025-12-03 16:26:48 +0530

    updated server.js for /form deployment

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
-app.use("/api", require("./routes/form"));
+// Serve frontend files under /form
+app.use("/form", express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req, res) => {
-  res.send("Aeronica Backend Running...");
+// API route under /form/api
+app.use("/form/api", require("./routes/form"));
+
+// Health check - open https://gist.aeronica.in/form
+app.get("/form", (req, res) => {
+  res.send("Aeronica Form Backend Running...");
 });
 
 const PORT = 5000;
